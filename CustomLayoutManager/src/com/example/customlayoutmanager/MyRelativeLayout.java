bb704dd4c660072a38b20adc05434f85d9314b67
Bert Van Rillaer <vanrillaer@gmail.com>
2013-09-03 13:13:57 +0200

    Snapping to top, bottom, left and right of screen fully functional.

--- CustomLayoutManager/src/com/example/customlayoutmanager/MyRelativeLayout.java
+++ CustomLayoutManager/src/com/example/customlayoutmanager/MyRelativeLayout.java
@@ -16,48 +16,49 @@ public class MyRelativeLayout extends RelativeLayout {
 
 	private int mDx;
 	private int mDy;
-	
+
 	private int mDrawableResize;
 	private int mResizeHandleHeight;
 	private int mResizeHandleWidth;
-	
+
 	private boolean mEditViewMode = false;
 	private RelativeLayout mTempRelativeLayout;
 
 	private View mSelectedView = null;
 	private LayoutParams mSelectedViewLayout;
-	private boolean mSelectFirstTime = false;
+
+	private boolean mSnappedHoriz;
+	private boolean mSnappedVert;
 	
-	//4 images used for handles in the selected view
+	private int mXSnapPosition;
+	private int mYSnapPosition;
+
+	// 4 images used for handles in the selected view
 	private View mTopHandle;
 	private View mLeftHandle;
 	private View mRightHandle;
 	private View mBottomHandle;
 
-	public MyRelativeLayout(Context context,int drawableResize) {
+	public MyRelativeLayout(Context context, int drawableResize) {
 		super(context);
 		mDrawableResize = drawableResize;
-		//get the size of the used image to get positioning right
+		// get the size of the used image to get positioning right
 		Drawable d = getResources().getDrawable(mDrawableResize);
 		mResizeHandleWidth = d.getIntrinsicWidth();
-		mResizeHandleHeight= d.getIntrinsicHeight();	
+		mResizeHandleHeight = d.getIntrinsicHeight();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public void setLayoutEditable(boolean layoutIsEditable){
-		mEditViewMode = true;		
+
+	public void setLayoutEditable(boolean layoutIsEditable) {
+		mEditViewMode = true;
 	}
 
-	public void addView(View v, int left, int top, int right, int bottom)
-			throws IllegalArgumentException {
+	public void addView(View v, int left, int top, int right, int bottom) throws IllegalArgumentException {
 		if (top >= bottom)
-			throw new IllegalArgumentException(
-					"Top cannot be greater than or equal to bottom");
+			throw new IllegalArgumentException("Top cannot be greater than or equal to bottom");
 		if (right <= left)
-			throw new IllegalArgumentException(
-					"Left cannot be greater than or equal to right");
-		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
-				right - left, bottom - top);
+			throw new IllegalArgumentException("Left cannot be greater than or equal to right");
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(right - left, bottom - top);
 		params.leftMargin = left;
 		params.topMargin = top;
 		v.setLayoutParams(params);
@@ -86,94 +87,126 @@ public class MyRelativeLayout extends RelativeLayout {
 
 			int x = (int) ev.getX();
 			int y = (int) ev.getY();
-			View v = getTouchedView(x,y);
-			Log.d("bert","touched view is " + v);
-			
+			View v = getTouchedView(x, y);
+			//Log.d("bert", "touched view is " + v);
+
 			switch (action) {
 			case (MotionEvent.ACTION_DOWN):
-				Log.d("bert","ACTION DOWN");
-				
-				//if you click on the upper relativelayout, unselect last selected
-				if(v == null && mTempRelativeLayout != null){
-					viewUnSelect(mTempRelativeLayout);
-				}
-
-				mSelectFirstTime = false;
-				if (v != null && v!=mTempRelativeLayout) {
-
-					if (mSelectedView != null) {
-						viewUnSelect(mSelectedView);
-					}
-				    mSelectFirstTime = true;	
-					
-					mSelectedView = v;
-					viewSelect(v);
-					
-
-					Log.d("bert", "touched" + v);
-					
-					
-					// mPressed = true;
-				}
-			
-				if (v != null) {
+				if (v == null) {
+					// if you click on the upper relativelayout, unselect last
+					// selected
+					// check if something was selected, if so remove handles
+					if (mTempRelativeLayout != null)
+						viewUnSelect(mTempRelativeLayout);
+				} else {
+					// user clicked on normal view inside relativelayout
+					Log.d("bert", "SNAPPING FALSE");
+					mSnappedHoriz = false;
+					mSnappedVert = false;
 					mDx = v.getLeft() - x;
 					mDy = v.getTop() - y;
+
+					// check if the view in focus has to change
+					if (v != mTempRelativeLayout) {
+						if (mSelectedView != null) {
+							viewUnSelect(mSelectedView);
+						}
+						viewSelect(v);
+
+					}
 				}
-				
-				
 				return true;
-				
+
 			case (MotionEvent.ACTION_MOVE):
-				Log.d("bert","ACTION MOVE");
-				if(mSelectFirstTime == false && mTempRelativeLayout!=null){
-					
+				Log.d("bert", "ACTION MOVE");
+				if (mTempRelativeLayout != null) {
+
 					LayoutParams layout = (LayoutParams) mTempRelativeLayout.getLayoutParams();
 					layout.leftMargin = x + mDx;
 					layout.topMargin = y + mDy;
+
+					// start unsnapping if possible
+					if(mSnappedVert){
+						Log.d("bert","into code to get things unsnapped");
+						Log.d("bert","mYSnapposition is " + mYSnapPosition);
+						if(Math.abs(mYSnapPosition-y) > mResizeHandleHeight){
+							mSnappedVert = false;
+							mTempRelativeLayout.setTop(mYSnapPosition);
+							this.requestLayout();
+							this.invalidate();
+							Log.d("bert","unsnapped it");
+							mTopHandle.setVisibility(View.VISIBLE);
+							mBottomHandle.setVisibility(View.VISIBLE);
+						}
+					}
 					
-					//Start snapping part
-					Log.d("bert","parent place right " + ((ViewGroup)mTempRelativeLayout.getParent()).getRight());
+					if(mSnappedHoriz){
+						Log.d("bert","into code to get things unsnapped");
+						Log.d("bert","mYSnapposition is " + mXSnapPosition);
+						if(Math.abs(mXSnapPosition-x) > mResizeHandleWidth){
+							mSnappedHoriz = false;
+							mTempRelativeLayout.setLeft(mXSnapPosition);
+							this.requestLayout();
+							this.invalidate();
+							Log.d("bert","unsnapped it");
+							mLeftHandle.setVisibility(View.VISIBLE);
+							mRightHandle.setVisibility(View.VISIBLE);
+						}
+					}
 					
-				
-					if(mTempRelativeLayout.getTop() + mResizeHandleHeight/2 <=0){
-						layout.topMargin = ((ViewGroup)mTempRelativeLayout.getParent()).getTop()-mResizeHandleHeight/2;
+
+					// Start snapping part
+					//Log.d("bert", "parent place right " + ((ViewGroup) mTempRelativeLayout.getParent()).getRight());
+					if (mTempRelativeLayout.getTop() + mResizeHandleHeight / 2 <= 0) {
+						layout.topMargin = ((ViewGroup) mTempRelativeLayout.getParent()).getTop() - mResizeHandleHeight / 2;
 						mTopHandle.setVisibility(View.INVISIBLE);
+						if(!mSnappedVert)
+							mYSnapPosition = y;
+							
+						mSnappedVert = true;
+						
 					}
-					if(mTempRelativeLayout.getLeft() + mResizeHandleWidth/2 <=0){
-						layout.leftMargin = ((ViewGroup)mTempRelativeLayout.getParent()).getLeft() -mResizeHandleWidth/2;
-						mLeftHandle.setVisibility(View.INVISIBLE);
-					}
-					Log.d("locking","right comp is " + (mTempRelativeLayout.getRight()) + " en parent right is " + ((ViewGroup)mTempRelativeLayout.getParent()).getRight());
-					//Log.d("locking","top comp is " + (mTempRelativeLayout.getTop()) + " en parent top is " + ((ViewGroup)mTempRelativeLayout.getParent()).getTop());
-					//Log.d("locking","bottom comp is " + (mTempRelativeLayout.getBottom()) + " en parent bottom is " + ((ViewGroup)mTempRelativeLayout.getParent()).getBottom());
-					if(mTempRelativeLayout.getRight()>=((ViewGroup)mTempRelativeLayout.getParent()).getRight() +mResizeHandleWidth/2){
-						//layout.rightMargin = 0;
-						mRightHandle.setVisibility(View.INVISIBLE);
-						layout.leftMargin = ((ViewGroup)mTempRelativeLayout.getParent()).getRight()-mTempRelativeLayout.getWidth() +mResizeHandleWidth/2;
-					}
-					if(mTempRelativeLayout.getBottom()>=((ViewGroup)mTempRelativeLayout.getParent()).getBottom()+mResizeHandleHeight/2){
-						layout.topMargin = ((ViewGroup)mTempRelativeLayout.getParent()).getBottom()-mTempRelativeLayout.getHeight()+mResizeHandleHeight/2;
+					if (mTempRelativeLayout.getBottom() >= ((ViewGroup) mTempRelativeLayout.getParent()).getBottom()
+							+ mResizeHandleHeight / 2) {
+						layout.topMargin = ((ViewGroup) mTempRelativeLayout.getParent()).getBottom()- mTempRelativeLayout.getHeight() + mResizeHandleHeight / 2;
 						mBottomHandle.setVisibility(View.INVISIBLE);
+						if(!mSnappedVert)
+							mYSnapPosition = y;
+						mSnappedVert = true;
 					}
 					
 					
+					if (mTempRelativeLayout.getLeft() + mResizeHandleWidth / 2 <= 0) {
+						layout.leftMargin = ((ViewGroup) mTempRelativeLayout.getParent()).getLeft() - mResizeHandleWidth / 2;
+						mLeftHandle.setVisibility(View.INVISIBLE);
+						if(!mSnappedHoriz)
+							mXSnapPosition = x;
+						mSnappedHoriz = true;
+					}
+					if (mTempRelativeLayout.getRight() >= ((ViewGroup) mTempRelativeLayout.getParent()).getRight()
+							+ mResizeHandleWidth / 2) {
+						mRightHandle.setVisibility(View.INVISIBLE);
+						layout.leftMargin = ((ViewGroup) mTempRelativeLayout.getParent()).getRight()
+								- mTempRelativeLayout.getWidth() + mResizeHandleWidth / 2;
+						if(!mSnappedHoriz)
+							mXSnapPosition = x;
+						mSnappedHoriz = true;
+					}
 					
-					//Stop snapping part
-					
+					// Stop snapping part
+
 					mTempRelativeLayout.setLayoutParams(layout);
-					//Log.d("bert","moved and top is now" + v.getTop());
-					
-					
+					// Log.d("bert","moved and top is now" + v.getTop());
+
 					this.requestLayout();
 					this.invalidate();
-					
+
 				}
 				return true;
 			case (MotionEvent.ACTION_UP):
-				Log.d("bert","ACTION UP");
+				Log.d("bert", "ACTION UP");
 
-				//mPressed = false;
+				// mPressed = false;
 				mDx = 0;
 				mDy = 0;
 				return true;
@@ -198,7 +231,7 @@ public class MyRelativeLayout extends RelativeLayout {
 		return super.onTouchEvent(ev);
 		// return false;
 	}
-	
+
 	/*
 	 * This Method has to be overridden because we want to handle touch events
 	 * ourselves in this class.
@@ -209,124 +242,115 @@ public class MyRelativeLayout extends RelativeLayout {
 	}
 
 	/*
-	 * Put the selected view in a relativelayout in order to get an overlay
-	 * on top of the view with handles for moving and resizing the view
+	 * Put the selected view in a relativelayout in order to get an overlay on
+	 * top of the view with handles for moving and resizing the view
 	 */
 	private void viewSelect(View v) {
 		// TODO Auto-generated method stub
+		// save clicked view as selected view
+		mSelectedView = v;
+
 		RelativeLayout newrel = new RelativeLayout(getContext());
 		mTempRelativeLayout = newrel;
 		removeView(v);
 		RelativeLayout.LayoutParams l = (LayoutParams) v.getLayoutParams();
-		//create a copy to save the inner view sizes until object deselected
+		// create a copy to save the inner view sizes until object deselected
 		mSelectedViewLayout = new LayoutParams(l);
 		RelativeLayout.LayoutParams innerViewLayout = new RelativeLayout.LayoutParams(l);
-		
+
 		int viewWidth = l.width;
 		int viewHeight = l.height;
-		
-		
-	
-		
-		//change layout params because we want the resize images centered on bound
+
+		// change layout params because we want the resize images centered on
+		// bound
 		l.width = l.width + mResizeHandleWidth;
 		l.height = l.height + mResizeHandleHeight;
-		l.leftMargin -= mResizeHandleWidth/2;
-		l.rightMargin -= mResizeHandleWidth/2;
-		l.topMargin -= mResizeHandleHeight/2;
-		l.bottomMargin -= mResizeHandleHeight/2;
-	
-		addView(newrel,l);
-		
-		//set alpha to show which view is in EDIT mode
+		l.leftMargin -= mResizeHandleWidth / 2;
+		l.rightMargin -= mResizeHandleWidth / 2;
+		l.topMargin -= mResizeHandleHeight / 2;
+		l.bottomMargin -= mResizeHandleHeight / 2;
+
+		addView(newrel, l);
+
+		// set alpha to show which view is in EDIT mode
 		v.setAlpha(0.2f);
-				
-		
-		
-		//copy view into inner layout
+
+		// copy view into inner layout
 		newrel.addView(v, innerViewLayout);
-		
-		
-		//inner view which contains the real view
-		innerViewLayout.leftMargin = mResizeHandleWidth/2;
-		innerViewLayout.topMargin = mResizeHandleWidth/2;
-		
-		//add left Resize image
-		RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
+
+		// inner view which contains the real view
+		innerViewLayout.leftMargin = mResizeHandleWidth / 2;
+		innerViewLayout.topMargin = mResizeHandleWidth / 2;
+
+		// add left Resize image
+		RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
 				RelativeLayout.LayoutParams.WRAP_CONTENT);
 		lp2.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
-		lp2.addRule(RelativeLayout.ALIGN_PARENT_LEFT,RelativeLayout.TRUE);
+		lp2.addRule(RelativeLayout.ALIGN_PARENT_LEFT, RelativeLayout.TRUE);
 		ImageView bullet = new ImageView(getContext());
 		bullet.setImageResource(R.drawable.bullet);
 		newrel.addView(bullet, lp2);
 		mLeftHandle = bullet;
-		
-		//add right Resize image
-		lp2 = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+		// add right Resize image
+		lp2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 		lp2.addRule(RelativeLayout.CENTER_VERTICAL, RelativeLayout.TRUE);
 		lp2.leftMargin = viewWidth;
-		bullet =  new ImageView(getContext());
+		bullet = new ImageView(getContext());
 		bullet.setImageResource(R.drawable.bullet);
-		mRightHandle= bullet;
-	
+		mRightHandle = bullet;
+
 		newrel.addView(bullet, lp2);
-		
-		//add top Resize image
-		lp2 = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+		// add top Resize image
+		lp2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 		lp2.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
 		lp2.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
 		bullet = new ImageView(getContext());
 		bullet.setImageResource(R.drawable.bullet);
 		newrel.addView(bullet, lp2);
 		mTopHandle = bullet;
-		
 
-		//add bottom Resize image
-		lp2 = new RelativeLayout.LayoutParams(
-				RelativeLayout.LayoutParams.WRAP_CONTENT,
-				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		// add bottom Resize image
+		lp2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
 		lp2.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
 		bullet = new ImageView(getContext());
 		bullet.setImageResource(R.drawable.bullet);
 		lp2.topMargin = viewHeight;
 		newrel.addView(bullet, lp2);
 		mBottomHandle = bullet;
-	
+
 		requestLayout();
 		invalidate();
-		Log.d("bert","component " + v + " has been selected");
+		Log.d("bert", "component " + v + " has been selected");
 
 	}
 
 	private void viewUnSelect(View v) {
 		// TODO Auto-generated method stub
-		
-		//get the size of the used image to get positioning right
+
+		// get the size of the used image to get positioning right
 		Drawable d = getResources().getDrawable(mDrawableResize);
 		int w = d.getIntrinsicWidth();
 		int h = d.getIntrinsicHeight();
-	
+
 		View viewInside = mTempRelativeLayout.getChildAt(0);
-		//RelativeLayout.LayoutParams layoutParams = (LayoutParams) rl.getLayoutParams();
-		//viewInside.setLayoutParams(mSelectedViewLayout);
+		// RelativeLayout.LayoutParams layoutParams = (LayoutParams)
+		// rl.getLayoutParams();
+		// viewInside.setLayoutParams(mSelectedViewLayout);
 		v.setAlpha(1f);
 		mTempRelativeLayout.removeAllViews();
 		removeView(mTempRelativeLayout);
-		mSelectedViewLayout.leftMargin = ((LayoutParams)mTempRelativeLayout.getLayoutParams()).leftMargin + w/2 ;
-		mSelectedViewLayout.topMargin = ((LayoutParams)mTempRelativeLayout.getLayoutParams()).topMargin + h/2;
-		addView(viewInside,mSelectedViewLayout);
-		Log.d("bert","component " + viewInside + " was removed from the rellayout container");
-		
-		//put null in mTempRelativeLayout because 2x clicking upper relativealyout (this) otherwise crashes
-		mTempRelativeLayout =null;
+		mSelectedViewLayout.leftMargin = ((LayoutParams) mTempRelativeLayout.getLayoutParams()).leftMargin + w / 2;
+		mSelectedViewLayout.topMargin = ((LayoutParams) mTempRelativeLayout.getLayoutParams()).topMargin + h / 2;
+		addView(viewInside, mSelectedViewLayout);
+		Log.d("bert", "component " + viewInside + " was removed from the rellayout container");
+
+		// put null in mTempRelativeLayout because 2x clicking upper
+		// relativealyout (this) otherwise crashes
+		mTempRelativeLayout = null;
 		mSelectedView = null;
-		
-		
+
 		requestLayout();
 		invalidate();
 	}
